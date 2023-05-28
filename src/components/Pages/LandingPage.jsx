e4073158be6d500865460f781c278d7a8e33aefb
Patrick MacIlwaine <patrick@macilwaine.com>
2023-05-28 13:13:46 -0500

    Landing Page Css

--- src/components/Pages/LandingPage.jsx
+++ src/components/Pages/LandingPage.jsx
@@ -6,28 +6,48 @@ import classes from './LandingPage.module.css';
 
 export default function LandingPage(){
 
-  const [input,setInput] = useState("");
+  const [password,setPassword] = useState("");
+  const [username, setUsername] = useState("Admin")
   let navigate = useNavigate();
 
 
   function handleClick(){
-    localStorage.setItem('isAllowed',input)
+    localStorage.setItem('isAllowed',password)
     let path = "/home"
     navigate(path);
-    
   }
 
 
   return(
+
     <div className={classes.body}>
-      <h1 className={classes.text}>ADMIN PASSWORD</h1>
-      <div>
-      <input className={classes.input}
-      type="text"
-      text={input}
-      onChange={e => setInput(e.target.value)}/>
-    <button className={classes.button} onClick={handleClick}>Submit</button>
-    </div>
+      <div className={classes.roundedPage}>
+        <h1 className={classes.text}>Sign in : </h1>
+        <form>
+          <div className={classes.colSpan}>
+            <label className={classes.label}>Name</label>
+            <div>
+            <input className={classes.input}
+            type="text"
+            defaultValue={username}
+            onChange={e => setUsername(e.target.value)}/>
+            </div>
+          </div>
+
+          <div className={classes.colSpan}>
+            <label className={classes.label}>Password</label>
+            <div>
+            <input className={classes.input}
+            type="text"
+            text={password}
+            onChange={e => setPassword(e.target.value)}/>
+            </div>
+          </div>
+
+
+        <button className={classes.button} onClick={handleClick}>Submit</button>
+        </form>
+      </div>
     </div>
   );
 
